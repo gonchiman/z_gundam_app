4abc3639d564a56c05857cf2304bbc08235e3711
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-06-27 20:48:35 +0900

    Add validation for crew member and captain availability in PilotAssignment and Warship models

--- app/models/pilot_assignment.rb
+++ app/models/pilot_assignment.rb
@@ -2,4 +2,16 @@ class PilotAssignment < ApplicationRecord
   belongs_to :warship
   belongs_to :crew_member
   belongs_to :mobile_suit, optional: true
+
+  validate :crew_member_must_be_pilot_available
+
+  private
+
+  def crew_member_must_be_pilot_available
+    return if crew_member.blank?
+
+    unless crew_member.role_type == "pilot_only" || crew_member.role_type == "captain_and_pilot"
+      errors.add(:crew_member_id, "must be pilot available")
+    end
+  end
 end
--- app/models/warship.rb
+++ app/models/warship.rb
@@ -2,4 +2,16 @@ class Warship < ApplicationRecord
   belongs_to :captain, class_name: "CrewMember", optional: true
 
   has_many :pilot_assignments, dependent: :destroy
+
+  validate :captain_must_be_available
+
+  private
+
+  def captain_must_be_available
+    return if captain_id.blank?
+
+    unless captain.role_type == "captain_only" || captain.role_type == "captain_and_pilot"
+      errors.add(:captain_id, "must be captain available")
+    end
+  end
 end
