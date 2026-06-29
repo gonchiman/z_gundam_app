e53175809f19bef97b71b13dcd5d27c9b4ffb857
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-06-29 17:21:25 +0900

    Add role column to users table for user role management

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_29_040557) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_29_081904) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -78,6 +78,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_29_040557) do
     t.string "email_address", null: false
     t.string "name"
     t.string "password_digest", null: false
+    t.string "role"
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
