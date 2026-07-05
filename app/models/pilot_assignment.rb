fe0c5fba8f96abe6ac34af366ce9639097942724
Yamamoto <107669839+gonchiman@users.noreply.github.com>
2026-07-05 14:03:51 +0900

    Validate assigned mobile suit was purchased by warship

--- app/models/pilot_assignment.rb
+++ app/models/pilot_assignment.rb
@@ -4,6 +4,7 @@ class PilotAssignment < ApplicationRecord
   belongs_to :mobile_suit, optional: true
 
   validate :crew_member_must_be_pilot_available
+  validate :mobile_suit_must_be_purchased_by_warship
   validates :crew_member_id, uniqueness: {
     scope: :warship_id,
     message: "はこの戦艦にすでに登録されています"
@@ -16,6 +17,14 @@ class PilotAssignment < ApplicationRecord
 
     unless crew_member.role_type == "pilot_only" || crew_member.role_type == "captain_and_pilot"
       errors.add(:crew_member_id, "must be pilot available")
+    end
+  end
+
+  def mobile_suit_must_be_purchased_by_warship
+    return if mobile_suit_id.blank? || warship.blank?
+
+    unless warship.mobile_suit_ids.include?(mobile_suit_id)
+      errors.add(:mobile_suit_id, "must be purchased by this warship")
     end
   end
 end
