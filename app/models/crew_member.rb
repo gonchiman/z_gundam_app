d240371dd6c1c9386247d667f8822c8f712edfba
Yamamoto <107669839+gonchiman@users.noreply.github.com>
2026-07-05 22:02:05 +0900

    Add pilot compatibility associations

--- app/models/crew_member.rb
+++ app/models/crew_member.rb
@@ -1,6 +1,14 @@
 class CrewMember < ApplicationRecord
   has_many :pilot_assignments
   has_many :compatibility_bonuses, dependent: :destroy
+  has_many :source_pilot_compatibility_bonuses,
+    class_name: "PilotCompatibilityBonus",
+    foreign_key: :source_crew_member_id,
+    dependent: :destroy
+  has_many :target_pilot_compatibility_bonuses,
+    class_name: "PilotCompatibilityBonus",
+    foreign_key: :target_crew_member_id,
+    dependent: :destroy
 
   has_one_attached :image
   has_one_attached :captain_warship_image
