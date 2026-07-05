95e493bfe5ff0e8825bdbcdaca7deb2214de0cab
Yamamoto <107669839+gonchiman@users.noreply.github.com>
2026-07-05 14:01:10 +0900

    Add warship association to mobile suit

--- app/models/mobile_suit.rb
+++ app/models/mobile_suit.rb
@@ -1,5 +1,7 @@
 class MobileSuit < ApplicationRecord
   has_many :pilot_assignments
+  has_many :warship_mobile_suits, dependent: :destroy
+  has_many :warships, through: :warship_mobile_suits
 
   has_one_attached :image
 end
