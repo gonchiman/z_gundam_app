2a8aefd55c0e88ec5e5561cf7ca3391ef5238d3d
Yamamoto <107669839+gonchiman@users.noreply.github.com>
2026-07-05 18:16:43 +0900

    Prevent buying mobile suits over budget

--- app/models/warship_mobile_suit.rb
+++ app/models/warship_mobile_suit.rb
@@ -7,4 +7,25 @@ class WarshipMobileSuit < ApplicationRecord
     scope: :warship_id,
     message: "はこの戦艦ですでに購入されています"
   }
+  validate :purchase_cost_must_fit_budget
+
+  private
+
+  def purchase_cost_must_fit_budget
+    return if warship.blank? || mobile_suit.blank?
+
+    cost = additional_purchase_cost
+    return if cost <= 0
+
+    if cost > warship.remaining_budget
+      errors.add(:base, "Budget exceeded")
+    end
+  end
+
+  def additional_purchase_cost
+    previous_quantity = quantity_in_database.to_i
+    added_quantity = quantity.to_i - previous_quantity
+
+    mobile_suit.purchase_cost.to_i * added_quantity
+  end
 end
