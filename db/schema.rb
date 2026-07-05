2b2a1b2a2dc6c74e99b0234d2f1bfbfbe3336ad6
Yamamoto <107669839+gonchiman@users.noreply.github.com>
2026-07-05 20:40:05 +0900

    Update schema for three million warship budget

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_05_090000) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_05_093000) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -99,7 +99,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_05_090000) do
   end
 
   create_table "warships", force: :cascade do |t|
-    t.integer "budget", default: 5000000, null: false
+    t.integer "budget", default: 3000000, null: false
     t.integer "captain_id"
     t.datetime "created_at", null: false
     t.string "name"
