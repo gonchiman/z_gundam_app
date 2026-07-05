4c367b0b830d29cfab23e0a5762391ae85d4c40d
Yamamoto <107669839+gonchiman@users.noreply.github.com>
2026-07-05 18:04:11 +0900

    Fix routes comment after adding warship mobile suit new route

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker
+  # get "service-worker" => "rails/pwa#service_worker"
 
   # Defines the root path route ("/")
   # root "posts#index"
