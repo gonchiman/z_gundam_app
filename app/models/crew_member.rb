0f3661c1ee96c503fe627384552862f03c817bbe
Yamamoto <107669839+gonchiman@users.noreply.github.com>
2026-07-05 22:13:03 +0900

    Add captain compatibility associations

--- app/models/crew_member.rb
+++ app/models/crew_member.rb
@@ -9,6 +9,14 @@ class CrewMember < ApplicationRecord
     class_name: "PilotCompatibilityBonus",
     foreign_key: :target_crew_member_id,
     dependent: :destroy
+  has_many :captain_compatibility_bonuses,
+    class_name: "CaptainCompatibilityBonus",
+    foreign_key: :captain_crew_member_id,
+    dependent: :destroy
+  has_many :target_captain_compatibility_bonuses,
+    class_name: "CaptainCompatibilityBonus",
+    foreign_key: :target_crew_member_id,
+    dependent: :destroy
 
   has_one_attached :image
   has_one_attached :captain_warship_image
