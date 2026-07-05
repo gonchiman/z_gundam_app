158a56925f01b3d57f85d09ffa0a9fc013d5dae9
Yamamoto <107669839+gonchiman@users.noreply.github.com>
2026-07-05 21:20:38 +0900

    Add mobile suit compatibility bonuses association

--- app/models/mobile_suit.rb
+++ app/models/mobile_suit.rb
@@ -2,6 +2,7 @@ class MobileSuit < ApplicationRecord
   has_many :pilot_assignments
   has_many :warship_mobile_suits, dependent: :destroy
   has_many :warships, through: :warship_mobile_suits
+  has_many :compatibility_bonuses, dependent: :destroy
 
   has_one_attached :image
 end
