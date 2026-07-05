d51c1209af390e6aa49d61aa31cb57be6584af28
Yamamoto <107669839+gonchiman@users.noreply.github.com>
2026-07-05 21:22:15 +0900

    Update schema for compatibility bonuses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_05_093000) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_05_100000) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -37,6 +37,18 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_05_093000) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "compatibility_bonuses", force: :cascade do |t|
+    t.integer "bonus_power", default: 0, null: false
+    t.integer "crew_member_id", null: false
+    t.datetime "created_at", null: false
+    t.string "description"
+    t.integer "mobile_suit_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["crew_member_id", "mobile_suit_id"], name: "index_compatibility_bonuses_on_crew_and_mobile_suit", unique: true
+    t.index ["crew_member_id"], name: "index_compatibility_bonuses_on_crew_member_id"
+    t.index ["mobile_suit_id"], name: "index_compatibility_bonuses_on_mobile_suit_id"
   end
 
   create_table "crew_members", force: :cascade do |t|
@@ -110,6 +122,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_05_093000) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "compatibility_bonuses", "crew_members"
+  add_foreign_key "compatibility_bonuses", "mobile_suits"
   add_foreign_key "pilot_assignments", "crew_members"
   add_foreign_key "pilot_assignments", "warships"
   add_foreign_key "sessions", "users"
