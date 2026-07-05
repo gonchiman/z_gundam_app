6471d57ad077a257f13f41688bfc9cb74c2f4635
Yamamoto <107669839+gonchiman@users.noreply.github.com>
2026-07-05 21:20:37 +0900

    Add crew member compatibility bonuses association

--- app/models/crew_member.rb
+++ app/models/crew_member.rb
@@ -1,5 +1,6 @@
 class CrewMember < ApplicationRecord
   has_many :pilot_assignments
+  has_many :compatibility_bonuses, dependent: :destroy
 
   has_one_attached :image
   has_one_attached :captain_warship_image
